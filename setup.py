76fc593b334bb27f795014085355ef1da680e20b
Óscar Nájera <najera.oscar@gmail.com>
2014-11-04 00:53:42 +0100

    pylint to setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ class PyTest(TestCommand):
     """Test class to do test coverage analysis"""
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        self.test_args = ['--cov-report', 'term-missing', '--cov', 'slaveparticles', 'tests/']
+        self.test_args = ['--cov-report', 'term-missing',
+                          '--cov', 'slaveparticles', 'tests/']
         self.test_suite = True
     def run_tests(self):
         #import here, cause outside the eggs aren't loaded
@@ -20,14 +21,14 @@ class PyTest(TestCommand):
         sys.exit(errno)
 
 setup(
-    name = "Slave Particles",
-    description = "Educative code on Slave Particles",
-    version = "0.2",
-    packages = find_packages(),
-    url = "https://github.com/Titan-C/slaveparticles",
-    author = "Óscar Nájera",
+    name="Slave Particles",
+    description="Educative code on Slave Particles",
+    version="0.2",
+    packages=find_packages(),
+    url="https://github.com/Titan-C/slaveparticles",
+    author="Óscar Nájera",
     author_email='najera.oscar@gmail.com',
-    license = "GNU General Public License v3 (GPLv3)",
+    license="GNU General Public License v3 (GPLv3)",
 
     install_requires=['numpy', 'scipy', 'Sphinx', 'matplotlib'],
 
