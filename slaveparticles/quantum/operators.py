bbab6922870d177fa369585420214b1bf60199a9
Óscar Nájera <najera.oscar@gmail.com>
2014-11-14 14:28:56 +0100

    New structure for DMFT 2 site in nicer OOP format

--- slaveparticles/quantum/operators.py
+++ slaveparticles/quantum/operators.py
@@ -11,51 +11,26 @@ from slaveparticles.quantum.spinmatrices import *
 
 def fermi_dist(energy, beta):
     """ Fermi Dirac distribution"""
-    return 1./(np.exp(beta*energy) +1)
+    return 1./(np.exp(beta*energy) + 1)
+
 
 def partition_func(beta, energies):
     """Evaluate the partition function for an array of energies"""
     return np.exp(-beta*energies).sum()
 
-#Operators
+
+# Operators
 def commutator(first, second):
     """Applies the commutation of two matrix operators"""
     return first.dot(second) - second.dot(first)
+
 
 def anticommutator(first, second):
     """Applies the anti-commutation of two matrix operators"""
     return first.dot(second) + second.dot(first)
 
-def fermion_phase(state, index):
-    """Counts the number of fermions present in the state before the indexed
-       one. Then returns the fermion phase sign"""
-    amount = bin(state >> index+1).count('1')
-    if amount % 2 == 0:
-        return 1
-    else:
-        return -1
 
-from scipy.sparse import csr_matrix
-def f_destruct(particles, index):
-    """Fermion annihilation operator in matrix representation for a indexed
-       particle in a bounded N-particles fermion fock space"""
-
-    mat = np.zeros((2**particles, 2**particles))
-
-    flipper = 2**index
-    for i in range(2**particles):
-        ispin = btest(i, index)
-        if ispin == 1:
-            mat[i ^ flipper, i] = fermion_phase(i, index)
-    return csr_matrix(mat)
-
-def f_creation(particles, index):
-    """Fermion creation operator in matrix representation for an indexed
-       particle in a bounded N-particles fermion fock space"""
-    return f_destruct(particles, index).T
-
-
-#Tools
+# Tools
 def diagonalize(operator):
     """diagonalizes single site Spin Hamiltonian"""
     eig_values, eig_vecs = LA.eigh(operator)
@@ -63,6 +38,21 @@ def diagonalize(operator):
     eig_values -= emin
 
     return eig_values, eig_vecs
+
+
+def gf_lehmann(eig_e, eig_states, d_dag, beta, omega):
+    """Outputs the lehmann representation of the greens function
+       omega has to be given, as matsubara frequencies or their analitical
+       continuation"""
+    zet = partition_func(beta, eig_e)
+    G = 0
+    for i in range(len(eig_e)):
+        for j in range(len(eig_e)):
+            G += np.dot(eig_states[:, j].T, d_dag.dot(eig_states[:, i]))**2 * \
+                       (np.exp(-beta*eig_e[i]) + np.exp(-beta*eig_e[j])) / \
+                       (omega + eig_e[i] - eig_e[j])
+    return G / zet
+
 
 def expected_value(operator, eig_values, eig_states, beta):
     """Calculates the average value of an observable
