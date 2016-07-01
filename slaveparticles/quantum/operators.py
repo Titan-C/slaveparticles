834c756667bd12e36f6413a7a9f2033e28190539
Óscar Nájera <najera.oscar@gmail.com>
2016-07-01 23:51:33 +0200

    Lehmann representation for complex hamiltonian

--- slaveparticles/quantum/operators.py
+++ slaveparticles/quantum/operators.py
@@ -11,13 +11,13 @@ from itertools import product
 
 def fermi_dist(energy, beta):
     """ Fermi Dirac distribution"""
-    exponent = np.asarray(beta*energy).clip(-600, 600)
-    return 1./(np.exp(exponent) + 1)
+    exponent = np.asarray(beta * energy).clip(-600, 600)
+    return 1. / (np.exp(exponent) + 1)
 
 
 def partition_func(beta, energies):
     """Evaluate the partition function for an array of energies"""
-    return np.exp(-beta*energies).sum()
+    return np.exp(-beta * energies).sum()
 
 
 # Operators
@@ -45,14 +45,14 @@ def gf_lehmann(eig_e, eig_states, d_dag, beta, omega, d=None):
     """Outputs the lehmann representation of the greens function
        omega has to be given, as matsubara or real frequencies"""
 
-    ew = np.exp(-beta*eig_e)
+    ew = np.exp(-beta * eig_e)
     zet = ew.sum()
     G = np.zeros_like(omega)
-    basis_create = np.dot(eig_states.T, d_dag.dot(eig_states))
+    basis_create = np.dot(eig_states.T.conj(), d_dag.dot(eig_states))
     if d is None:
-        tmat = np.square(basis_create)
+        tmat = np.square(np.abs(basis_create))
     else:
-        tmat = np.dot(eig_states.T, d.T.dot(eig_states))*basis_create
+        tmat = np.dot(eig_states.T.conj(), d.T.dot(eig_states)) * basis_create
 
     tmat *= np.add.outer(ew, ew)
     gap = np.add.outer(-eig_e, eig_e)
@@ -66,7 +66,7 @@ def gf_lehmann(eig_e, eig_states, d_dag, beta, omega, d=None):
 def expected_value(operator, eig_values, eig_states, beta):
     """Calculates the average value of an observable
        it requires that states and operators have the same base"""
-    aux = np.einsum('i,ji,ji', np.exp(-beta*eig_values),
-                    eig_states, operator.dot(eig_states))
+    aux = np.einsum('i,ji,ji', np.exp(-beta * eig_values),
+                    eig_states.conj(), operator.dot(eig_states))
 
     return aux / partition_func(beta, eig_values)
